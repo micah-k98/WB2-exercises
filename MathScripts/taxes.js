984d4c0bd1602ed87bb979ec542a38eebd686f44
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-18 11:39:57 -0700

    Added and done with taxes exercise

---
+++ MathScripts/taxes.js
@@ -0,0 +1,10 @@
+"use strict"
+
+const taxRate = .23;
+const salaryPerMonth = 3000;
+
+let withheldAmount = (salaryPerMonth * taxRate) * 12;
+console.log("The total Federal withheld for taxes for the whole year is $" + withheldAmount.toFixed(2)) ;
+
+const grossIncome = (salaryPerMonth * 12) - withheldAmount;
+console.log("The total gross income is $" + grossIncome);
