c6c9bdb2237127be5d3706e945f1e164fee83660
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-18 11:11:28 -0700

    Added and done with converting celsius to fahrenheit

---
+++ MathScripts/c_to_f.js
@@ -0,0 +1,6 @@
+"use strict"
+
+const celsius = 37;
+
+let fahrenheit = (9 / 5) * celsius + 32;
+console.log("Celsius of " + celsius + " degrees is equal to " + fahrenheit.toFixed(2) + " degrees fahrenheit");
