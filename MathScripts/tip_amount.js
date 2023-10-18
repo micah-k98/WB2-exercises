9b9d1c08fc85d8d01d4f379621247270d4d25c70
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-18 10:25:14 -0700

    Added and done with tip amount js file

---
+++ MathScripts/tip_amount.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const foodBill = 100;
+const tipPercentage = .20;
+
+let tipAmount = foodBill * tipPercentage;
+console.log("The tip on a $" + foodBill + " food bill is $" + tipAmount)
