9dfb3ae3adfc0907a9a3a6d67cf84f6b94c7de22
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-18 10:54:12 -0700

    Added and done with rule of 72 exercise

---
+++ MathScripts/rule_of_72.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const interestRate = 6;
+const doubledSavings =  100000;
+
+let doubleTimeInYears = 72 / interestRate;
+console.log("At a " + interestRate + "% interest rate, your savings account will be worth $" + doubledSavings + " in " + doubleTimeInYears);
