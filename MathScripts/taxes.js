e1dbf5536cf3d4ff0dec3ca4e34ea824d577b639
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-18 11:40:33 -0700

    Updated the output

--- MathScripts/taxes.js
+++ MathScripts/taxes.js
@@ -7,4 +7,4 @@ let withheldAmount = (salaryPerMonth * taxRate) * 12;
 console.log("The total Federal withheld for taxes for the whole year is $" + withheldAmount.toFixed(2)) ;
 
 const grossIncome = (salaryPerMonth * 12) - withheldAmount;
-console.log("The total gross income is $" + grossIncome);
+console.log("The total gross income is $" + grossIncome.toFixed(2));
