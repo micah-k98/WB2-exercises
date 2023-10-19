6330fd8be9e81176dc32b8fa022ef500cade236a
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-19 14:03:00 -0700

    Updated and added equal operator

--- IfScripts/min_max.js
+++ IfScripts/min_max.js
@@ -1,32 +1,34 @@
 "use strict"
 
-const a = 96;
-const b = 67;
-const c = 12;
+const a = 60;
+const b = 60;
+const c = 60;
 
 let smallestNumber = 0;
 let largestNumber = 0;
 
 
-if (a > b && a > c) {
+if (a >= b && a >= c) {
     largestNumber = a;
 }
-else if (b > a && b > c) {
+else if (b >= a && b >= c) {
     largestNumber = b;
 }
-else if (c > a && c > b) {
+else if (c >= a && c >= b) {
     largestNumber = c;
 }
 
-if (a < b && a < c) {
+if (a <= b && a <= c) {
     smallestNumber = a;
 }
-else if (b < a && b < c) {
+else if (b <= a && b <= c) {
     smallestNumber = b;
 }
-else if (c < a && c < b) {
+else if (c <= a && c <= b) {
     smallestNumber = c;
 }
+
+
 
 console.log("The largest number is " + largestNumber);
 console.log("The smallest number is " + smallestNumber);
