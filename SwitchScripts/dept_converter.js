54e1a18a833827f9270d98e93e397abb02c3e121
micah-k98 <146677653+micah-k98@users.noreply.github.com>
2023-10-19 14:45:07 -0700

    Added and done with dept_converter js file

---
+++ SwitchScripts/dept_converter.js
@@ -0,0 +1,28 @@
+"use strict"
+
+let departmentCode = 20;
+// let departmentName = "";
+
+switch (departmentCode)
+{
+    case 1:
+        console.log("1 - Marketing");
+        break;
+    case 5:
+        console.log("5 - Human Resources");
+        break;
+    case 10:
+        console.log("10 - Accounting");
+        break;
+    case 12:
+        console.log("12 - Legal");
+        break;
+    case 18:
+        console.log("18 - IT");
+        break;
+    case 20:
+        console.log("20 - Customer Relations");
+        break;
+    default:
+        console.log("<unknown>");
+}
